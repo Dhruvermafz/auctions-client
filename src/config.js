2879c57a010fa31c5480fa438b09542d27d50ca0
dhruvermafz <vermadhruv09112002@gmail.com>
2024-02-29 12:41:59 +0530

    hjvgc

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-let REACT_APP_API_BASE_URL = "https://auction-api-xbhd.onrender.com";
+let REACT_APP_API_BASE_URL = "http://localhost:8000";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  REACT_APP_API_BASE_URL = "https://auction-api-xbhd.onrender.com";
+  REACT_APP_API_BASE_URL = "http://localhost:8000";
 }
 
-const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/live-auctions/uploads";
-const UPLOAD_PRESET = "hgvapsg0";
-
-export { REACT_APP_API_BASE_URL, CLOUDINARY_URL, UPLOAD_PRESET };
+export { REACT_APP_API_BASE_URL };
